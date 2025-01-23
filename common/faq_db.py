c4a491167107744d8b6f93afa13fcc82d54732c2
hseonjo <coco7kiti@gmail.com>
2025-01-23 09:50:11 +0900

    faq_250126-0950

---
+++ common/faq_db.py
@@ -0,0 +1,4 @@
+# faq db에서 데이터 뽑기.
+from common.sql_constant import SELECT_SQLs
+import streamlit as st
+from sqlalchemy import text
---
+++ common/sql_constant.py
@@ -0,0 +1,11 @@
+import enum
+class SELECT_SQLs(enum.Enum):
+    # 1.FAQ 만 뽑아내기
+    FAQ_DATA = (enum.auto(),
+                """select * from faq""",
+                "FAQ 뽑아내기")  
+
+    # 2.검색 단어와 유사한 질의응답 뽑아내기
+    SIMILAR_DATA = (enum.auto(),
+                    """select * from faq like %{search_word}%??""",
+                    "검색단어와 유사한 질의응답 뽑아내기")
